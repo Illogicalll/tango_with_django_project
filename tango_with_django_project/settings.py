0357613fefdec08ff795d6578972b872897cd9d6
Will Murphy <2766915M@student.gla.ac.uk>
2023-02-09 12:40:06 +0000

    Ex 9 Completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -37,6 +37,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/rango/login/'
 
 # Application definition
 
@@ -105,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
